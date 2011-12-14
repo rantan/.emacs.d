7cd9cae9ad5de41f0c436f23a3898fdacfbf0b71
rantan <the.rantan@gmail.com>
2011-12-14 14:07:35 +0900

    ruby-モードを追加

--- init-ruby.el
+++ init-ruby.el
@@ -1,4 +1,11 @@
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/ruby-mode/"))
+(require 'ruby-mode)
 
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+
+(setq ruby-indent-level 2)
+(setq ruby-indent-tabs-mode nil)
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
