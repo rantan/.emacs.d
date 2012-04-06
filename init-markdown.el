f947b8227ad3e6fea69735a945b44c05373904c2
rantan <the.rantan@gmail.com>
2012-04-06 11:38:26 +0900

    なんかいろいろいじった

--- init-markdown.el
+++ init-markdown.el
@@ -5,8 +5,9 @@
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
-   (cons '("\\.md" . markdown-mode) auto-mode-alist))
+   (cons '("\\.md" . gfm-mode) auto-mode-alist))
 
 (setq markdown-command "/usr/local/bin/markdown")
+(setq markdown-css-path "/Users/rantan/.emacs.d/site-lisp/markdown-mode/markdown-css/markdown.css")
 
 (provide 'init-markdown)
--- init-moccur.el
+++ init-moccur.el
@@ -1,4 +1,21 @@
 (require 'color-moccur)
 (require 'moccur-edit)
 
+
+;; moccur-grep で特定のファイルを対象外としたい
+(setq moccur-grep-exclusion-buffer-name-regexp "\\(TAGS\\)\\|\\(\\.ext\\)|\\(\\.svn\\)|\\(\\.git\\)")
+
+(defun delete-string-match (reg lst)
+  (let ((ret nil))
+    (while lst
+      (unless (string-match reg (car lst))
+        (setq ret (cons (car lst) ret)))
+      (setq lst (cdr lst)))
+    (reverse ret)))
+(defadvice moccur-search-files (before moccur-search-files-with-exclusion)
+  "enable moccur-grep-exclusion-buffer-name-regexp"
+  (ad-set-arg 1 (delete-string-match moccur-grep-exclusion-buffer-name-regexp (ad-get-arg 1))))
+(ad-enable-advice 'moccur-search-files 'before 'moccur-search-files-with-exclusion)
+(ad-activate 'moccur-search-files)
+
 (provide 'init-moccur)
